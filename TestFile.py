c6589b5f7840238cd6174f159e1a6682d0314e25
KrunalPatel1107 <connectkrunalpatel@gmail.com>
2022-09-22 16:39:58 -0300

    Guess Game V1

--- TestFile.py
+++ TestFile.py
@@ -1,1 +1,16 @@
-print ("Hello World!")
+import random
+
+guesscount = 0
+guesslimit = 5
+
+randomnumber = random.randint(0, 10)
+
+
+while guesscount < guesslimit:
+    user_input = int(input("Guess a number: "))
+    guesscount += 1
+    if user_input == randomnumber:
+        print("You guessed the correct number!")
+        break
+    else:
+        print("Sorry, Try Again!")
